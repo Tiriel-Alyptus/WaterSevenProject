a2f7da2be669d4fe54d6da6a47ff42f19bcb8600
Tiriel-Alyptus <lorenzo.decaudin@gmail.com>
2023-05-31 17:25:10 +0200

    UP

--- WaterSeven/app.vue
+++ WaterSeven/app.vue
@@ -1,10 +1,14 @@
 <script> 
-import Header from './components/header.vue'
-import './components/header.css'
+import Header from './components/header/header.vue'
+import Body from './components/body/body.vue'
+import './components/body/body.css'
+import './components/header/header.css'
 </script>
 <template>
   <div>
     <Header />
+    <Body />
   </div>
 </template>
+
 
